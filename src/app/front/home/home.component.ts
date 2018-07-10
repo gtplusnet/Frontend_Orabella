2d078c0afb57269c60b52778b82cc87b93f20098
Mark Figues <markcarlofigues.digima@gmail.com>
2018-07-10 16:22:31 +0800

    swiper

--- src/app/front/home/home.component.ts
+++ src/app/front/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
+import Swiper from "swiper";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,91 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+  	this.gallery_swiper();
+  	this.product_swiper();
+  }
+  // Gallery Carousel
+  gallery_swiper() : void
+  {
+      var swiper = new Swiper('.g-carousel-container', 
+      {
+          slidesPerView: 4,
+          autoplay:
+          {
+              delay: 2500,
+              disableOnInteraction: false
+          },
+          spaceBetween: 16,
+
+          breakpoints: {
+              1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 40
+              },
+              768: {
+                  slidesPerView: 3,
+                  spaceBetween: 30
+              },
+              640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20
+              },
+              320: {
+                  slidesPerView: 1,
+                  spaceBetween: 10
+              }
+          },
+
+         // Navigation arrows
+         navigation: {
+             nextEl: '.swiper-button-next',
+             prevEl: '.swiper-button-prev',
+         },
+
+     });
+  }
+  // Product Listing Carousel
+  product_swiper() : void
+  {
+      var swiper = new Swiper('.product-container', 
+      {
+          slidesPerView: 4,
+          loop: true,
+          autoplay:
+          {
+              delay: 2500,
+              disableOnInteraction: false,
+          },
+          spaceBetween: 16,
+
+          breakpoints: {
+              1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+              },
+              768: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+              },
+              640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+              },
+              320: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+              }
+          },
+
+         // Navigation arrows
+         navigation: {
+             nextEl: '.swiper-button-next',
+             prevEl: '.swiper-button-prev',
+         },
+
+     });
   }
 
 }
